1f059a493be456e2818353a365f8008c289da736
moein <moein.mousavi1378@gmail.com>
2022-08-12 12:55:55 +0430

    commit for production

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: [
-    'vuetify'
-  ]
+    publicPath: '/art-gallery/',
+    transpileDependencies: [
+        'vuetify'
+    ]
 })
